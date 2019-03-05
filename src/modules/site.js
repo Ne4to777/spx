5dffe61c0ae8f3a44cb5d96e785745ee0c5747b4
Alex <ne4to777@gmail.com>
2019-03-05 19:05:24 +0300

    fix

--- src/modules/site.js
+++ src/modules/site.js
@@ -48,9 +48,7 @@ site.recycleBin = recycleBin(site);
 
 site.get = execute('properties')(identity);
 site.getCustomListTemplates = execute('customListTemplates')(substitutionI(pipe([getContext, getWeb]))(getListTemplates));
-site.getWebTemplates = execute('webTemplates')(spParentObject => {
-  return spParentObject.getWebTemplates(1033, 0)
-});
+site.getWebTemplates = execute('webTemplates')(spParentObject => spParentObject.getWebTemplates(1033, 0));
 
 window.spx = site;
 export default site;
