e567cc46636a0f0a3d3309e761fb149e355b61fb
Alex <ne4to777@gmail.com>
2019-03-11 12:45:22 +0300

    copy

---
+++ config/webpack.copy.js
@@ -0,0 +1,12 @@
+const config = require('./private.json');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  plugins: [new CopyPlugin([{
+    from: '**/*',
+    to: config.path + '/project',
+    ignore: ['node_modules/**']
+  },], {
+      copyUnmodified: true
+    })]
+};
