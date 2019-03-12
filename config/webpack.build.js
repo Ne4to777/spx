eacd291651b80ac81fc17c3a27d8baf3056e2713
Alex <ne4to777@gmail.com>
2019-03-12 15:08:40 +0300

    publish

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -13,7 +13,7 @@ module.exports = {
 	},
 	output: {
 		filename: config.filename,
-		path: path.resolve(__dirname, '../dist'),
+		path: path.resolve(__dirname, '../publish'),
 		library: config.library,
 		libraryTarget: 'umd'
 	}
