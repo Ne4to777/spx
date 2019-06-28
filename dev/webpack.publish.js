0c5a6b2dd5477c79942734ed77bd5ceaae865aaf
Alexeev Alexey <ne4to777@gmail.com>
2019-06-28 17:21:10 +0300

    babel

--- dev/webpack.publish.js
+++ dev/webpack.publish.js
@@ -4,7 +4,15 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  entry: './src/modules/site.js',
+  entry: ['babel-polyfill', './src/modules/site.js'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
   output: {
     filename: config.filename,
     path: path.resolve(__dirname, '../publish'),
