8483a2d7c0791fbb94fb3647ca54fc1b521ca2a3
Alexey Alexeev <ne4to777@gmail.com>
2019-08-07 10:52:10 +0300

    config

--- jest.config.js
+++ jest.config.js
@@ -169,7 +169,8 @@ module.exports = {
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
 	transformIgnorePatterns: [
 		'\\\\node_modules\\\\',
-		'<rootDir>/setupFiles/'
+		'<rootDir>/setupFiles/',
+		'<rootDir>/publish/'
 	],
 
 	// An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
